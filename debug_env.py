a13bd15fc7d32f1552781b3ffeeaf692c0ee89a1
JPCODESZ <jonpeppy2000@gmail.com>
2025-04-16 10:46:51 -0400

    Update debug_env.py

--- debug_env.py
+++ debug_env.py
@@ -1,1 +1,10 @@
+import os
+import time
 
+print("✅ STARTING DEBUG ENV TEST")
+
+print("APCA_API_KEY_ID:", os.getenv("APCA_API_KEY_ID"))
+print("APCA_API_SECRET_KEY:", os.getenv("APCA_API_SECRET_KEY"))
+print("APCA_API_BASE_URL:", os.getenv("APCA_API_BASE_URL"))
+
+time.sleep(60)
